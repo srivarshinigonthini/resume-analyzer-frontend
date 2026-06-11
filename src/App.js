42dc4276cd207b29599ef5b8c30ff4147ea625c0
srivarshinigonthini <srivarshinigonthini@gmail.com>
2026-06-11 12:21:22 +0530

    fix app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, { useState } from "react";
 import UploadPage from "./components/UploadPage";
 import ResultsPage from "./components/ResultsPage";
+import LoadingPage from "./components/LoadingPage";
 
 function App() {
   const [result, setResult] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   return (
     <div>
-      {result ? (
+      {loading ? (
+        <LoadingPage />
+      ) : result ? (
         <ResultsPage data={result} onBack={() => setResult(null)} />
       ) : (
-        <UploadPage onResult={setResult} />
+        <UploadPage onResult={setResult} onLoading={setLoading} />
       )}
     </div>
   );
